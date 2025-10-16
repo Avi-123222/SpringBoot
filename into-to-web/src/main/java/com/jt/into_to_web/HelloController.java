0f63826d1d84eb59e5f1cf1822b91ea17f528be0
Avi-123222 <dasavishek669@gmail.com>
2025-10-16 07:56:19 +0530

    About Thymeleaf and spring web dependency

--- into-to-web/src/main/java/com/jt/into_to_web/HelloController.java
+++ into-to-web/src/main/java/com/jt/into_to_web/HelloController.java
@@ -3,7 +3,11 @@ package com.jt.into_to_web;
 import java.io.PrintWriter;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class HelloController {
     @RequestMapping(path="/hello")
@@ -20,6 +24,22 @@ public class HelloController {
         writer.println("login successful");
 
     }
+    @RequestMapping({"/","/home"})
+    public String home(){
+        System.out.println("This is home page");
+        return "home";
+
+    }
+    
+    @RequestMapping({"/contact"})
+    public String contact(Model model){
+        model.addAttribute("name", "Java Technocrat");
+        model.addAttribute("branch", "Java with SpringBoot");
+        model.addAttribute("faculty", "Mr. Sai");
+        System.out.println("Hi everyone!!!!");
+        return "contact";
+
+    } 
 
 
 
