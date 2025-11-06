8ce781712463d9d81b246377bcd7b60cc41ef294
Avi-123222 <dasavishek669@gmail.com>
2025-11-06 21:14:26 +0530

    integrated JPA in our program

--- jt-blogs/src/main/java/com/jt/jt_blogs/model/Blog.java
+++ jt-blogs/src/main/java/com/jt/jt_blogs/model/Blog.java
@@ -1,6 +1,11 @@
 package com.jt.jt_blogs.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,7 +14,11 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Builder
 public class Blog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String heading;
     private String description;
