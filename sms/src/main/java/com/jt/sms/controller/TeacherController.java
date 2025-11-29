a5bf9a5ea5a47af1673b64e6f3bc761dd400226a
Avi-123222 <dasavishek669@gmail.com>
2025-11-29 08:37:52 +0530

    intro to loggin

--- sms/src/main/java/com/jt/sms/controller/TeacherController.java
+++ sms/src/main/java/com/jt/sms/controller/TeacherController.java
@@ -3,15 +3,25 @@ package com.jt.sms.controller;
 import com.jt.sms.model.Teacher;
 import com.jt.sms.service.TeacherService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/teacher")
+@Slf4j
 public class TeacherController {
     private final TeacherService service;
+
     @PostMapping("/create")
     public Teacher createTeacher(@RequestBody Teacher newTeacher){
+        log.trace("TRACE -> Saving1 teacher" + newTeacher);
+        log.debug("DEBUG -> Saving1 teacher" + newTeacher);
+        log.info("INFO -> Saving1 teacher" + newTeacher);
+        log.warn("WARN -> Saving1 teacher" + newTeacher);
+        log.error("ERROR-> Saving1 teacher" + newTeacher);
         return service.saveTeacher(newTeacher);
     }
     @PutMapping("/update/{id}")
@@ -19,3 +29,10 @@ public class TeacherController {
         return service.updatedTeacherById(id,toBeUpdated);
     }
 }
+/*
+* TRACE-> TRACE,DEBUG,INFO,WARN,ERROR
+* DEBUG-> DEBUG,INFO,WARN,ERROR
+* INFO(default)-> INFO,WARN,ERROR
+* WARN-> WARN,ERROR
+* ERROR->ERROR
+*/
